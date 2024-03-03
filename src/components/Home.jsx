cf82ad56e1f68c102f6a095cb46d1b2778230fcc
nishantkumar1308 <nishantsingh1308@gmail.com>
2024-03-03 13:43:21 +0530

    card updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 function Home() {
-  return <div>Card Design Practise "/test"</div>;
+  return (
+    <div>
+      <div className="flex justify-center text-slate-400 m-4 text-2xl  font-semibold">
+        Try Different Responsive Card Designs
+      </div>
+      <div className="flex justify-center gap-x-4 ">
+        <a href="/test" className="text-white bg-blue-500 rounded-full p-2">
+          1
+        </a>
+        <a href="/card2" className="text-white bg-blue-500 rounded-full p-2">
+          2{" "}
+        </a>
+        <a href="/card3" className="text-white bg-blue-500 rounded-full p-2">
+          3
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
